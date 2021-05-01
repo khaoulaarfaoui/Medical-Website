3c368364034ca02f1e529032f878ef9f55b67d84
khaoula <alarfaouikhaoula@gmail.com>
2021-05-01 21:37:44 +0100

    adding emploie

--- src/app/carriere/carriere.component.ts
+++ src/app/carriere/carriere.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carriere.component.css']
 })
 export class CarriereComponent implements OnInit {
-
+  focus: any;
+  focus1: any;
   constructor() { }
 
   ngOnInit(): void {
